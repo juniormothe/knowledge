6cf367be387949674ba20bf204a02f21173989ff
JUNIOR <junior.mothe@gmail.com>
2023-03-18 19:12:49 -0300

    api-whatsapp

--- class/Curl.php
+++ class/Curl.php
@@ -27,10 +27,10 @@ class Curl
             CURLOPT_POSTFIELDS => '' . $shippingData . '',
             CURLOPT_HTTPHEADER => array(
                 'Content-Type: application/json',
-                'SecretKey: e3b0e4b8-7670-47b6-8543-47f869ccc90e',
-                'PublicToken: e101ed3e-f52b-4214-9fd0-a755cbc1f733',
-                'DeviceToken: b232b361-b114-4256-a768-a8e6c2dc12ab',
-                'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3BsYXRhZm9ybWEuYXBpYnJhc2lsLmNvbS5ici9zb2NpYWwvZ2l0aHViL2NhbGxiYWNrIiwiaWF0IjoxNjc5MDc2NTU1LCJleHAiOjE3MTA2MTI1NTUsIm5iZiI6MTY3OTA3NjU1NSwianRpIjoiaHZZQ2V3d3V5MXpRUDR6YyIsInN1YiI6IjE2NTAiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.nJ4kuOdrMMrGwdEmIAWlYLB_0i6g9ZWnXbrsYWGKKvs'
+                'SecretKey: AVAILABLE_IN_API',
+                'PublicToken: AVAILABLE_IN_API',
+                'DeviceToken: AVAILABLE_IN_API',
+                'Authorization: Bearer AVAILABLE_IN_API'
             ),
         ));
         $response = curl_exec($curl);
