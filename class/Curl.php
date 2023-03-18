94563dd8c7f7cacbe888a0ac88ae437a3aa5a9dd
JUNIOR <junior.mothe@gmail.com>
2023-03-18 19:04:06 -0300

    curl-api-whatsapp

--- class/Curl.php
+++ class/Curl.php
@@ -7,18 +7,40 @@ namespace Classe;
  */
 class Curl
 {
-    public function test()
+    public function whatsapp($number, $message)
     {
+        $shippingData = json_encode([
+            'number' => "55" . str_replace([' ', '(', ')', '-'], "", $number),
+            'text' => $message
+        ]);
+
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "https://swapi.dev/api/films/1/");
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, "nome=Junior&sexo=1&idade=37");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $return = json_decode(curl_exec($curl));
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://cluster.apigratis.com/api/v1/whatsapp/sendText',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '' . $shippingData . '',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'SecretKey: e3b0e4b8-7670-47b6-8543-47f869ccc90e',
+                'PublicToken: e101ed3e-f52b-4214-9fd0-a755cbc1f733',
+                'DeviceToken: b232b361-b114-4256-a768-a8e6c2dc12ab',
+                'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3BsYXRhZm9ybWEuYXBpYnJhc2lsLmNvbS5ici9zb2NpYWwvZ2l0aHViL2NhbGxiYWNrIiwiaWF0IjoxNjc5MDc2NTU1LCJleHAiOjE3MTA2MTI1NTUsIm5iZiI6MTY3OTA3NjU1NSwianRpIjoiaHZZQ2V3d3V5MXpRUDR6YyIsInN1YiI6IjE2NTAiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.nJ4kuOdrMMrGwdEmIAWlYLB_0i6g9ZWnXbrsYWGKKvs'
+            ),
+        ));
+        $response = curl_exec($curl);
         curl_close($curl);
-        //
-        echo "<pre>";
-        var_dump($return);
-        echo "</pre>";
+        $result = json_decode($response, TRUE);
+
+        if (!$result['error']) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 }
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require('config/Config.php');
+require('function/GlobalFunctions.php');
+require('config/Autoload.php');
+//////////
+require('views/include/header.php');
+//////////
+
+/*
+$Curl = new Classe\Curl();
+
+if ($Curl->whatsapp('(21) 99251-6158', "HTTP_HOST: " . $_SERVER['HTTP_HOST'] . PHP_EOL . "PLATFORM: " . $_SERVER['HTTP_SEC_CH_UA_PLATFORM'] . PHP_EOL . "DOCUMENT: " . $_SERVER['DOCUMENT_ROOT'] . PHP_EOL . "REQUEST TIME: " . $_SERVER['REQUEST_TIME'])) {
+    echo "&#10004; Mensagem enviado com sucesso!";
+} else {
+    echo "&#10008; Erro ao enviar mensagem!";
+}
+*/
+
+//////////
+require('views/include/footer.php');
