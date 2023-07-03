11cdd54701bb34dc2c4f867eeb7549dd6a9845e8
Junior Mothé <62271652+juniormothe@users.noreply.github.com>
2023-07-03 09:24:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ require('views/include/header.php');
 //////////
 
 /*
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+	$uri = 'https://';
+} else {
+	$uri = 'http://';
+}
+
+
+
 $Curl = new Classe\Curl();
 
 if ($Curl->whatsapp('(21) 99251-6158', "HTTP_HOST: " . $_SERVER['HTTP_HOST'] . PHP_EOL . "PLATFORM: " . $_SERVER['HTTP_SEC_CH_UA_PLATFORM'] . PHP_EOL . "DOCUMENT: " . $_SERVER['DOCUMENT_ROOT'] . PHP_EOL . "REQUEST TIME: " . $_SERVER['REQUEST_TIME'])) {
